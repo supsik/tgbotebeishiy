853a4e2ded6c0997939e73b4dd38259223ebcebc
Sattsaev Zaur <o2@MacBook-Pro-Sattsaev.local>
2024-01-17 17:18:32 +0300

    add test token

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 TelegramApi = require('node-telegram-bot-api')
 
 const token = "6837522885:AAFqVLYVD-F53R04OFX6N5YxidV1s8qxHfs";
+const testToken = "6936294673:AAHI0oR6pL9LEHRV-EjapVwRNnr4ywUwZFA";
 
 const bot = new TelegramApi(token, {polling: true});
 
